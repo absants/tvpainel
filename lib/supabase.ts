627ddc74c41edc0f0f1cb3da0c9ef8ed584c5367
Antonio Bruno <antoniobruuno@gmail.com>
2025-06-05 01:05:22 -0300

    atualização

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://<SEU-PROJETO>.supabase.co';
-const supabaseKey = '<SUA-CHAVE-ANON>';
+const supabaseUrl = 'https://dtrqvlgdovwqfomgcjiw.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0cnF2bGdkb3Z3cWZvbWdjaml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwOTUyMDYsImV4cCI6MjA2NDY3MTIwNn0.D2EW7h0Yo-99qOzr_fuIMNXcsSwh_nY0N_mN4iLe9eo';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
